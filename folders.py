0e303d352904fd6706ebd740d18eb3f40ee174c3
Денчик <98116130+denischagin@users.noreply.github.com>
2022-02-03 20:07:45 +0300

    функция dictionary

--- folders.py
+++ folders.py
@@ -5,8 +5,15 @@ def way():
     pass
 
 
-def dictionary():
-    pass
+def dictionary(path):
+    result = {}
+    for filename in os.listdir(path):
+        current_puth = os.path.join(path, filename)
+        if os.path.isdir(current_puth):
+            result.update(dictionary(current_puth))
+        else:
+            result[current_puth] = os.path.getsize(current_puth)
+    return result
 
 
 def duplicate():
